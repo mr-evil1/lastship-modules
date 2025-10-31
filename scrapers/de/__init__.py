8538e8cf7d02e45a666c05b1b22945a4e7a9991a
mr-evil1 <vavootopro@gmail.com>
2025-10-31 10:09:04 +0100

    Modify encoding and add package comment
    Updated encoding declaration and added a package comment.

--- scrapers/de/__init__.py
+++ scrapers/de/__init__.py
@@ -1,9 +1,2 @@
-# -*- coding: UTF-8 -*-
-
-import os.path
-
-sourcePath = os.path.dirname(__file__)
-
-files = os.listdir(os.path.dirname(__file__))
-
-__all__ = [filename[:-3] for filename in files if not filename.startswith('__') and filename.endswith('.py')]
+# -*- coding: utf-8 -*-
+# Scraper Package
