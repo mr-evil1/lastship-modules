7fef0597cb4acb59546c0c327ca57d284fb7397f
mr-evil1 <vavootopro@gmail.com>
2026-02-27 10:04:06 +0100

    Refactor load and showEntries functions for clarity

--- sites/hdfilme.py
+++ sites/hdfilme.py
@@ -30,40 +30,77 @@ URL_KINO = URL_MAIN + 'aktuelle-kinofilme-im-kino/'
 URL_SERIES = URL_MAIN + 'serienstream-deutsch/'
 URL_SEARCH = URL_MAIN + 'index.php?do=search&subaction=search&story=%s'
 
+
 def load():
-    oNavigator.addDirectoryItem("Neu", 'runPlugin&site=%s&function=showEntries&sUrl=%s' % (SITE_NAME, URL_NEW), SITE_ICON, 'DefaultMovies.png')
-    oNavigator.addDirectoryItem("Kino", 'runPlugin&site=%s&function=showEntries&sUrl=%s' % (SITE_NAME, URL_KINO), SITE_ICON, 'DefaultMovies.png')
+    oNavigator.addDirectoryItem("Neu",    'runPlugin&site=%s&function=showEntries&sUrl=%s' % (SITE_NAME, URL_NEW),    SITE_ICON, 'DefaultMovies.png')
+    oNavigator.addDirectoryItem("Kino",   'runPlugin&site=%s&function=showEntries&sUrl=%s' % (SITE_NAME, URL_KINO),   SITE_ICON, 'DefaultMovies.png')
     oNavigator.addDirectoryItem("Serien", 'runPlugin&site=%s&function=showEntries&isTvshow=True&sUrl=%s' % (SITE_NAME, URL_SERIES), SITE_ICON, 'DefaultTVShows.png')
-    oNavigator.addDirectoryItem("Suche", 'runPlugin&site=%s&function=showSearch' % SITE_NAME, SITE_ICON, 'DefaultAddonsSearch.png')
+    oNavigator.addDirectoryItem("Suche",  'runPlugin&site=%s&function=showSearch' % SITE_NAME, SITE_ICON, 'DefaultAddonsSearch.png')
     oNavigator._endDirectory()
-
-
 
 
 def showEntries(entryUrl=False, sSearchText=False):
-    if not entryUrl: entryUrl = params.getValue('sUrl')
+    if not entryUrl:
+        entryUrl = params.getValue('sUrl')
+    isTvshow = params.getValue('isTvshow')
+
     sHtmlContent = cRequestHandler(entryUrl, ignoreErrors=True).request()
     pattern = '<div class="box-product(.*?)<h3.*?href="([^"]+).*?">([^<]+).*?(.*?)</li>'
     isMatch, aResult = cParser().parse(sHtmlContent, pattern)
-    if not isMatch: return
+    if not isMatch:
+        oNavigator._endDirectory()
+        return
+
+    # xsDirectory statt addDirectoryItem → vollständiges Kontextmenü
+    # navigatorXS.py muss isFolder=True für getHosters-Items setzen (siehe Fix)
+    items = []
     for sInfo, sUrl, sName, sDummy in aResult:
-        if sSearchText and not cParser.search(sSearchText, sName): continue
+        if sSearchText and not cParser.search(sSearchText, sName):
+            continue
         isThumbnail, sThumbnail = cParser().parseSingleResult(sInfo, 'data-src="([^"]+)')
-        if isThumbnail and sThumbnail.startswith('/'): sThumbnail = URL_MAIN + sThumbnail
-        u = 'runPlugin&site=%s&function=getHosters&sUrl=%s&sThumbnail=%s&sTitle=%s' % (
-            SITE_NAME, quote_plus(sUrl if sUrl.startswith('http') else URL_MAIN + sUrl.lstrip('/')),
-            quote_plus(sThumbnail), quote_plus(sName)
-        )
-        oNavigator.addDirectoryItem(sName, u, sThumbnail, 'DefaultVideo.png')
+        if isThumbnail and sThumbnail.startswith('/'):
+            sThumbnail = URL_MAIN + sThumbnail
+        full_url = sUrl if sUrl.startswith('http') else URL_MAIN + sUrl.lstrip('/')
+        items.append({
+            'title':     sName,
+            'infoTitle': sName,
+            'entryUrl':  full_url,
+            'sUrl':      full_url,
+            'poster':    sThumbnail,
+            # isTvshow=True erzwingt isFolder=True in xsDirectory →
+            # getHosters bekommt echten Handle statt -1
+            'isTvshow':  True,
+            'sFunction': 'getHosters',
+        })
+
+    oNavigator.xsDirectory(items, SITE_NAME)
+
     isMatchNext, sNextUrl = cParser().parseSingleResult(sHtmlContent, 'href="([^"]+)">›</a>')
     if isMatchNext:
-        oNavigator.addDirectoryItem('>>> Nächste Seite', 'runPlugin&site=%s&function=showEntries&sUrl=%s' % (SITE_NAME, sNextUrl), SITE_ICON, 'DefaultMovies.png')
+        oNavigator.addDirectoryItem('>>> Nächste Seite',
+            'runPlugin&site=%s&function=showEntries&sUrl=%s' % (SITE_NAME, sNextUrl),
+            SITE_ICON, 'DefaultMovies.png')
     oNavigator._endDirectory()
 
+
 def getHosters():
+    # Läuft jetzt mit echtem Handle (isTvshow=True → isFolder=True in xsDirectory)
     sUrl = params.getValue('sUrl')
     sThumbnail = params.getValue('sThumbnail')
     sTitle = params.getValue('sTitle')
+    sMeta = params.getValue('meta') or ''
+
+    # meta-Fallback: xsDirectory übergibt Parameter als JSON in 'meta'
+    if not sUrl:
+        if sMeta:
+            try:
+                meta = json.loads(sMeta)
+            except Exception:
+                meta = {}
+            sUrl       = meta.get('entryUrl') or meta.get('sUrl') or ''
+            sThumbnail = sThumbnail or meta.get('poster') or ''
+            sTitle     = sTitle or meta.get('infoTitle') or meta.get('title') or ''
+
     sHtmlContent = cRequestHandler(sUrl).request()
     patterns = [
         'link="([^"]+)',
@@ -78,53 +115,30 @@ def getHosters():
             found_links.extend(results)
     if not found_links:
         xbmcgui.Dialog().notification(SITE_NAME, "Keine Links gefunden", SITE_ICON, 3000)
+        oNavigator._endDirectory()
         return
+
     progressDialog.create(SITE_NAME, 'Prüfe Hoster...')
     processed_links = list(set(found_links))
     total = len(processed_links)
-    valid_found = False
     exclude = ['youtube', 'youtu.be', 'trailer', 'facebook', 'gstatic', '.ttf', '.woff', '.svg', '.css', '.js']
 
+    # showHosters-Format: [sHosterName, sTitle, meta, isResolve, sUrl, sThumbnail]
+    hosters = []
     for i, hUrl in enumerate(processed_links):
         if progressDialog.iscanceled(): break
         if hUrl.startswith('//'): hUrl = 'https:' + hUrl
-        hUrl_lower = hUrl.lower()
-        if any(x in hUrl_lower for x in exclude): continue
-        if 'embed-.html' in hUrl_lower: continue
+        if not hUrl.startswith('http'): continue
+        if any(x in hUrl.lower() for x in exclude): continue
+        if 'embed-.html' in hUrl.lower(): continue
         if resolveurl and resolveurl.HostedMediaFile(hUrl).valid_url():
-            valid_found = True
             hName = hUrl.split('/')[2].replace('www.', '').split('.')[0].capitalize()
-            u = 'runPlugin&site=%s&function=play&sUrl=%s&sTitle=%s' % (SITE_NAME, quote_plus(hUrl), quote_plus(sTitle))
-            oNavigator.addDirectoryItem(hName, u, sThumbnail, 'DefaultVideo.png', isFolder=False)
+            hosters.append([hName, sTitle, sMeta, False, hUrl, sThumbnail])
         progressDialog.update(int((i / total) * 100))
-    progressDialog.close()
-    if not valid_found:
-        xbmcgui.Dialog().notification(SITE_NAME, "Keine validen Streams", SITE_ICON, 3000)
-    oNavigator._endDirectory()
 
-def play():
-    sUrl = params.getValue('sUrl')
-    sTitle = params.getValue('sTitle')
-    if not resolveurl:
-        xbmcgui.Dialog().notification(SITE_NAME, "ResolveURL fehlt!", SITE_ICON, 5000)
-        return
-    progressDialog.create(SITE_NAME, 'Löse Stream auf...')
-    stream_url = None
-    try:
-        stream_url = resolveurl.resolve(sUrl)
-    except Exception as e:
-        logger.info("ResolveURL Error: %s" % str(e))
-        progressDialog.close()
-        xbmcgui.Dialog().notification(SITE_NAME, "Hoster-Fehler (404/Timeout)", SITE_ICON, 5000)
-        return
     progressDialog.close()
-    if stream_url:
-        listItem = xbmcgui.ListItem(sTitle)
-        listItem.setPath(stream_url)
-        # Wiedergabe für Kodi 21
-        xbmc.Player().play(stream_url, listItem)
-    else:
-        xbmcgui.Dialog().notification(SITE_NAME, "Stream nicht gefunden", SITE_ICON, 5000)
+    oNavigator.showHosters(json.dumps(hosters))
+
 
 def showSearch():
     text = oNavigator.showKeyBoard()
