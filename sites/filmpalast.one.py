5d4ecabdb70b40771c45663299a00e346127ef42
mr-evil1 <vavootopro@gmail.com>
2026-07-06 19:07:11 +0200

    Add initial setup for filmpalast.one integration

--- sites/filmpalast.one.py
+++ sites/filmpalast.one.py
@@ -1,1 +1,20 @@
 
+import json, sys,xbmcgui,re
+from resources.lib.ParameterHandler import ParameterHandler
+from resources.lib.requestHandler import cRequestHandler
+from resources.lib.tools import logger, cParser
+from resources.lib.control import progressDialog, quote_plus, unescape, quote, execute
+from resources.lib.indexers.navigatorXS import navigator
+from resources.lib.utils import isBlockedHoster
+from resources.lib.control import getSetting, setSetting
+oNavigator = navigator()
+addDirectoryItem = oNavigator.addDirectoryItem
+setEndOfDirectory = oNavigator._endDirectory
+xsDirectory = oNavigator.xsDirectory
+params = ParameterHandler()
+SITE_IDENTIFIER = 'filmpalast.one'
+SITE_NAME = 'Filmpalast_One'
+SITE_ICON = 'filmpalast.png'
+import xbmcgui
+def load():
+    xbmcgui.Dialog().notification('xStreamV2','Noch nicht im xStream V2 verfügbar')
